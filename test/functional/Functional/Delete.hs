9dff8477b1ae771907cee6eb3449aa7c93f72cd2
Tommy Bidne <tbidne@protonmail.com>
2024-07-15 14:34:40 +1200

    Add delete many test

--- test/functional/Functional/Delete.hs
+++ test/functional/Functional/Delete.hs
@@ -12,6 +12,7 @@ tests testEnv =
     [ testDeleteNone testEnv,
       testDeleteOne testEnv,
       testDeleteGroup testEnv,
+      testDeleteMany testEnv,
       testDeleteGroupSubtask testEnv,
       testDeleteBadId testEnv,
       testDeleteBlockedId testEnv
@@ -23,77 +24,85 @@ testDeleteNone =
     (runTodoResponses ["n"])
     "Does not delete a task"
     [osp|testDeleteNone|]
-    "walk_dog"
+    ["walk_dog"]
 
 testDeleteOne :: IO TestEnv -> TestTree
 testDeleteOne =
-  testDeleteGolden "Deletes a task" [osp|testDeleteOne|] "walk_dog"
+  testDeleteGolden "Deletes a task" [osp|testDeleteOne|] ["walk_dog"]
 
 testDeleteGroup :: IO TestEnv -> TestTree
 testDeleteGroup =
-  testDeleteGolden "Deletes a group" [osp|testDeleteGroup|] "soccer_match"
+  testDeleteGolden "Deletes a group" [osp|testDeleteGroup|] ["soccer_match"]
+
+testDeleteMany :: IO TestEnv -> TestTree
+testDeleteMany =
+  testDeleteGolden
+    "Deletes several task"
+    [osp|testDeleteMany|]
+    ["haircut", "cleats", "bananas"]
 
 testDeleteGroupSubtask :: IO TestEnv -> TestTree
 testDeleteGroupSubtask =
-  testDeleteGolden "Deletes a group subtask" [osp|testDeleteGroupSubtask|] "cleats"
+  testDeleteGolden "Deletes a group subtask" [osp|testDeleteGroupSubtask|] ["cleats"]
 
 testDeleteBadId :: IO TestEnv -> TestTree
 testDeleteBadId =
   testDeleteErrorGolden
     "Delete non-extant id fails"
     [osp|testDeleteBadId|]
-    "bad_id"
+    ["bad_id"]
 
 testDeleteBlockedId :: IO TestEnv -> TestTree
 testDeleteBlockedId =
   testDeleteErrorGolden
     "Delete task that blocks others fails"
     [osp|testDeleteBlockedId|]
-    "fix_car"
+    ["fix_car"]
 
-testDeleteGolden :: TestName -> OsPath -> String -> IO TestEnv -> TestTree
+testDeleteGolden :: TestName -> OsPath -> List String -> IO TestEnv -> TestTree
 testDeleteGolden = testDeleteGoldenRunner (runTodoResponses ["y"])
 
-testDeleteErrorGolden :: TestName -> OsPath -> String -> IO TestEnv -> TestTree
+testDeleteErrorGolden :: TestName -> OsPath -> List String -> IO TestEnv -> TestTree
 testDeleteErrorGolden = testDeleteGoldenRunner (runTodoException @DeleteE)
 
 testDeleteGoldenRunner ::
   (List String -> IO Text) ->
   TestName ->
   OsPath ->
-  String ->
+  List String ->
   IO TestEnv ->
   TestTree
-testDeleteGoldenRunner runner desc name taskId testEnv = goldenVsFile desc goldenPath actualPath $ do
-  testDir <- getTestDir' testEnv name
-  let newPath = testDir </> [osp|index.json|]
-      deleteArgs =
-        [ "--index-path",
-          unsafeDecodeOsToFp newPath,
-          "--color",
-          "off",
-          "delete",
-          taskId
-        ]
+testDeleteGoldenRunner runner desc name taskIds testEnv =
+  goldenVsFile desc goldenPath actualPath $ do
+    testDir <- getTestDir' testEnv name
+    let newPath = testDir </> [osp|index.json|]
+        deleteArgs =
+          [ "--index-path",
+            unsafeDecodeOsToFp newPath,
+            "--color",
+            "off",
+            "delete"
+          ]
+            ++ taskIds
 
-  -- copy example to test dir
-  copyFileWithMetadata exampleJson newPath
+    -- copy example to test dir
+    copyFileWithMetadata exampleJson newPath
 
-  -- run delete
-  deleteResult <- runner deleteArgs
+    -- run delete
+    deleteResult <- runner deleteArgs
 
-  let listArgs =
-        [ "--index-path",
-          unsafeDecodeOsToFp newPath,
-          "--color",
-          "off",
-          "list"
-        ]
+    let listArgs =
+          [ "--index-path",
+            unsafeDecodeOsToFp newPath,
+            "--color",
+            "off",
+            "list"
+          ]
 
-  -- run list
-  listResult <- runTodo listArgs
+    -- run list
+    listResult <- runTodo listArgs
 
-  writeActualFile actualPath (deleteResult <> "\n\n" <> listResult)
+    writeActualFile actualPath (deleteResult <> "\n\n" <> listResult)
   where
     path = outputDir `cfp` unsafeDecodeOsToFp name
     actualPath = path <> ".actual"
