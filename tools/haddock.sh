dc9a218fb70b70d02792dd96076233231c940567
Tommy Bidne <tbidne@protonmail.com>
2024-07-27 17:02:20 +1200

    Improve haddock script

--- tools/haddock.sh
+++ tools/haddock.sh
@@ -2,7 +2,6 @@ set -e
 
 export LANG="C.UTF-8"
 
-cabal update
 cabal haddock --haddock-hyperlink-source --haddock-quickjump
 
 mkdir -p docs/
@@ -10,4 +9,4 @@ mkdir -p docs/
 # shellcheck disable=SC2038
 find docs/ -type f | xargs -I % sh -c "rm -r %"
 
-cp -r dist-newstyle/build/x86_64-linux/ghc-*/todo-0.1/opt/doc/html/todo/* docs/
+cp -r dist-newstyle/build/x86_64-linux/ghc-*/todo-*/opt/doc/html/todo/* docs/
